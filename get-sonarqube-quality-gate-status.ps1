1d9e49650aefd16e4ecf190aa96cc8dba2e6e00f
Lucas.Christinson <Lucas.Christinson@nordsonasymtek.com>
2018-11-12 13:36:38 -0800

    Updating build pipeline

--- get-sonarqube-quality-gate-status.ps1
+++ get-sonarqube-quality-gate-status.ps1
@@ -1,22 +1,3 @@
-# This script checks for Quality Gate status for a SonarQube Project
-[CmdletBinding()]
-Param(
-    # Define Sonar Token
-    [Parameter (Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String] $SonarToken,
-
-    # Define SonarQube Server URI
-    [Parameter (Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String] $SonarServerName,
-
-    # Define Project Key
-    [Parameter (Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String] $SonarProjectKey
-)
-
 $Token = [System.Text.Encoding]::UTF8.GetBytes("32bbef2a150290c6c38101e99b7dd4d861af0d61" + ":")
 $TokenInBase64 = [System.Convert]::ToBase64String($Token)
  
